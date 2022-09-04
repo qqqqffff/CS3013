f6a859cac98d9d3329ce997b03b31e2ad1fc32f8
apollo <1apollo.rowe@gmail.com>
2022-09-04 15:12:25 -0400

    section 4 completion

--- project1/slug.c
+++ project1/slug.c
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
+#include <math.h>
 
 int main(int argc, char *argv[]){
 
@@ -26,6 +27,10 @@ int main(int argc, char *argv[]){
     FILE *fp;
     char seed_file[32] = "seed_slug_";
     strcat(strcat(seed_file, argv[1]), ".txt");
+    if(atoi(argv[1]) > 4 || atoi(argv[1]) < 1){
+        printf("%d, is not in the allowed range", atoi(argv[1]));
+        exit(EXIT_FAILURE);
+    }
     fp = fopen(seed_file, "r");
     if(fp == NULL){
         printf("File: %s, failed to open", seed_file);
@@ -36,18 +41,31 @@ int main(int argc, char *argv[]){
     printf("[Slug #%d]: Read seed: %d, From %s\n\n", getpid(), seed , seed_file);
     srand(seed);
 
-    char *command;
+    char *cmd = NULL;
+    char **cmdv;
+    char *command = NULL;
     int time = (rand() % 5) + 4;
     int coinflip = rand() % 2;
     if(coinflip == 1){
+        cmd = "id";
+        cmdv = calloc(3, sizeof(char *));
+        cmdv[0] = "id";
+        cmdv[1] = "-g";
+        cmdv[2] = NULL;
         command = "id --group";
     }
     else{
+        cmd = "last";
+        cmdv = calloc(4, sizeof(char *));
+        cmdv[0] = "last";
+        cmdv[1] = "-i";
+        cmdv[2] = "-x";
+        cmdv[3] = NULL;
         command = "last -i -x";
     }
     printf("[Slug #%d]: I'll take %d seconds. Coin flip: %d\n", getpid(), time, coinflip);
     sleep(time);
     printf("[Slug #%d]: Breaktime over: executing \'%s\' command...\n", getpid(), command);
-    system(command);
+    execvp(cmd, cmdv);
     exit(0);
 }
