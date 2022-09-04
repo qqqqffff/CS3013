41cec529f94a08068f95f9bc92c75477b6ebf5cd
apollo <1apollo.rowe@gmail.com>
2022-09-04 17:19:38 -0400

    slugrace wip

--- project1/slugrace.c
+++ project1/slugrace.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+#include <math.h>
 
 int main(){
+    int rc = fork();
+    if(rc == 0){
+        for(int i = 1; i < 5; i++){
+            char *slugnumb = malloc(sizeof(char) * (int)log10(i));
+            sprintf(slugnumb, "%d", i);
+            char **cmdv;
+            char *cmd = "./slug";
+            cmdv = calloc(3, sizeof(char *));
+            cmdv[0] = "./slug";
+            cmdv[1] = slugnumb;
+            cmdv[2] = NULL;
+            execvp(cmd, cmdv);
+        }
+    }else if(rc > 0){
+        // struct timespec start, finish, delta;
+        // clock_gettime(CLOCK_REALTIME, &start);
+
+        // int status;
+        // printf("[Parent #%d]: I am waiting for %d to finish...\n", rc, getpid());
+        // if(waitpid(rc, &status, 0) == -1){
+        //     exit(EXIT_FAILURE);
+        // }
+        // else{
+        //     int exit_code = WEXITSTATUS(status);
+        //     printf("[Parent #%d]: Child #%d finished with code %d\n", getpid(), rc, exit_code);
+        //     exit(0);
+        // }
+    }else{
+        printf("Error forking");
+        exit(EXIT_FAILURE);
+    }
+
+
     exit(0);
 }
