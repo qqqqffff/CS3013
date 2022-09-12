5ca1cd07c7110b415e1d70cd3ffef624fef0f166
apollo <1apollo.rowe@gmail.com>
2022-09-12 17:34:44 -0400

    Player Generation

--- project2/OmniSportsComplex.c
+++ project2/OmniSportsComplex.c
@@ -11,14 +11,19 @@ int baseball_players = 0;
 int soccer_players = 0;
 int football_players = 0;
 
-int activeSport = 0;
+int active_baseball_players = 0;
+int active_soccer_players = 0;
+int active_football_players = 0;
 
-sem_t athlete_gen_semaphore;
+int active_sport = 0;
+char *active_sport_val;
+
+sem_t athlete_semaphore;
 
 void *generateAthlete(void* arg){
-    int sport_val = *(int*) arg;
+    int sport_val = rand() % 3;;
     char *sport = NULL;
-    sem_wait(&athlete_gen_semaphore);
+    sem_wait(&athlete_semaphore);
     if(sport_val == 2){
         soccer_players++;
         sport = "Soccer";
@@ -29,7 +34,6 @@ void *generateAthlete(void* arg){
         baseball_players++;
         sport = "Baseball";
         printf("[Stadium]; Generating a baseball player, total number of players: %d\n", baseball_players);
-        
     }
     else if(sport_val == 0){
         football_players++;
@@ -40,10 +44,36 @@ void *generateAthlete(void* arg){
     else{
         printf("[Stadium]: Unable to create specified player\n");
     }
-    sem_post(&athlete_gen_semaphore);
+    sem_post(&athlete_semaphore);
     int arrival_time = rand() % 4 + 1;
     sleep(arrival_time);
-    printf("\t[%s Player]: I arrived to the complex in %d seconds\n", sport, arrival_time);
+    sem_wait(&athlete_semaphore);
+    if(!active_sport){
+        printf("\t[%s Player]: I arrived to the complex in %d seconds!\n", sport, arrival_time);
+    }
+    sem_post(&athlete_semaphore);
+
+    sem_wait(&athlete_semaphore);
+    if(sport_val == 2){
+        active_soccer_players++;
+        if(!active_sport){
+            printf("[Stadium]: Total number of active %s players: %d\n", sport, active_soccer_players);
+        }
+    }
+    else if(sport_val == 1){
+        active_baseball_players++;
+        if(!active_sport){
+            printf("[Stadium]: Total number of active %s players: %d\n", sport, active_baseball_players);
+        }
+    }
+    else if(sport_val == 0){
+        active_football_players++;
+        if(!active_sport){
+            printf("[Stadium]: Total number of active %s players: %d\n", sport, active_football_players);
+        }
+    }
+    sem_post(&athlete_semaphore);
+    // while(!active_sport);
     return NULL;
 }
 
@@ -62,18 +92,24 @@ int main(){
     printf("[Stadium]: Read seed: %d, From %s\n\n", seed , seed_file);
     srand(seed);
 
-    sem_init(&athlete_gen_semaphore, 0, 1);
+    // sem_init(&athlete_gen_semaphore, 0, 1);
     pthread_t* thread_id = malloc(124 * sizeof(pthread_t));
     int counter = 0;
     
-    for(int i = 0; i < 4; i++){
-        int sport_val = rand() % 3;
-        pthread_create(&thread_id[i], NULL, generateAthlete, &sport_val);
+    for(int i = 0; i < 124; i++){
+        pthread_create(&thread_id[i], NULL, generateAthlete, NULL);
+        usleep(250000);
         counter++;
+        if(active_baseball_players > 2){
+            active_sport = 1;
+            active_sport_val = "BASEBALL";
+            printf("\n\n[Stadium]: Ready for a game of BALL?, Field Configuration: %s\n\n", active_sport_val);
+            break; //TODO: replace with gamestart thread
+        }
     }
-    for(int i = 0; i < 4; i++){
+    for(int i = 0; i < counter; i++){
         pthread_join(thread_id[i], NULL);
     }
-    sem_destroy(&athlete_gen_semaphore);
+    // sem_destroy(&athlete_gen_semaphore);
     exit(0);
 }
